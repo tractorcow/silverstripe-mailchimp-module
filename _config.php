68194de6aa30f73a041ff8fa94bd195d3d5662cb
Joe Harvey <joe.harvey@quadradigital.co.uk>
2016-02-23 17:38:28 +0000

    Adding Creative Commons 'Attribute, Non-Commercial' licence to all code files and a licence.md file with a copyright notice and disclaimer

--- _config.php
+++ _config.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * This work is licensed under the Creative Commons Attribution-NonCommercial 4.0 International License.
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc/4.0/.
+ */
+
 // Extensions
 Object::add_extension('Member', 'MCMemberExtension');
 Object::add_extension('SiteConfig', 'MCSiteConfigExtension');
